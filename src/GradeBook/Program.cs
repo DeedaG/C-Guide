384cc0a3316e298f63d3bfbce7bdc07b4e209954
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-12-16 09:01:00 -0600

    save c# guide

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var book = new Book("Sott's grade book");
+
+            while(true)
+            {
+                Console.WriteLine("Please enter a grade or q to quit");
+                var input = Console.ReadLine();
+
+                if(input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
+            
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"The average grade is {stats.Average}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+    }
+}
